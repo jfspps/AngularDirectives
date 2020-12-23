cc7468d281ec0be3cbf7be2ba08d45c1d02876fe
jfspps <jfsapps@gmail.com>
2020-12-23 13:20:36 +0000

    added HostListener DOM event response

--- src/app/advanced-highlight/advanced-highlight.directive.ts
+++ src/app/advanced-highlight/advanced-highlight.directive.ts
@@ -1,10 +1,9 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appAdvancedHighlight]'
 })
 export class AdvancedHighlightDirective implements OnInit{
-
   // legacy code may contain RendererV2 instead of Renderer2
   // https://angular.io/api/core/Renderer2
   constructor(private elRef : ElementRef, private renderer: Renderer2) { }
@@ -12,6 +11,16 @@ export class AdvancedHighlightDirective implements OnInit{
   // this approach does not directly access DOM objects as basic-highlight does and is also considered better practice
   // it works without a browser (basicHighlight would return an error attempting to access the DOM if a browser was used)
   ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  // the argument mouseenter is an official DOM event
+  // https://developer.mozilla.org/en-US/docs/Web/Events
+  @HostListener('mouseenter') mouseover() {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
   }
 }
