409047d80859f8412e741e40620b6fd9c607d411
jfspps <jfsapps@gmail.com>
2020-12-23 13:41:53 +0000

    binding @Input with directive properties

--- src/app/advanced-highlight/advanced-highlight.directive.ts
+++ src/app/advanced-highlight/advanced-highlight.directive.ts
@@ -1,12 +1,16 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAdvancedHighlight]'
 })
 export class AdvancedHighlightDirective implements OnInit{
+
+  // one can also set aliases to input properties and then use [appAdvancedHighlight]="'red'" to denote the directive and property at the same time (see https://angular.io/guide/attribute-directives#bind-to-an-input-alias)
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
   
   // pass the DOM property and sub-properties (cf. basic-highlight directive's backgroundColor)
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   // legacy code may contain RendererV2 instead of Renderer2
   // https://angular.io/api/core/Renderer2
@@ -16,18 +20,19 @@ export class AdvancedHighlightDirective implements OnInit{
   // it works without a browser (basicHighlight would return an error attempting to access the DOM if a browser was used)
   ngOnInit() {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
   }
 
   // the argument mouseenter is an official DOM event
   // https://developer.mozilla.org/en-US/docs/Web/Events
   @HostListener('mouseenter') mouseover() {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave() {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 }
